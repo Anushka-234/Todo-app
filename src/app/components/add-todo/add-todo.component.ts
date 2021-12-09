32350d6bae67395e7ed6d5f465b50d8f050ab431
anushka23 <anushkabhattarai@bajratechnologies.com>
2021-12-09 22:18:08 +0545

    initial design for todo app

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,35 +1,32 @@
-import { Component, OnInit,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/todo';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.scss']
+  styleUrls: ['./add-todo.component.scss'],
 })
 export class AddTodoComponent implements OnInit {
-	
-	title: string;
-	day: string;
-  active:boolean=false;
-    @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  title: string;
+  day: string;
+  active: boolean = false;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit(){
-  	const todo = {
-  	 sno : 8,
-  	 title : this.title,
-  	 day : this.day,
-  	 active : true
-  	}
-  	this.todoAdd.emit(todo);
+  onSubmit() {
+    const todo = {
+      sno: 8,
+      title: this.title,
+      day: this.day,
+      active: true,
+    };
+    this.todoAdd.emit(todo);
 
-    this.title="";
-    this.day = "";
+    this.title = '';
+    this.day = '';
     this.active = false;
   }
-
 }
