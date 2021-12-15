6ff90ce1e538cbf67fe2bc8ae15623fd839887ff
anushka23 <anushkabhattarai@bajratechnologies.com>
2021-12-15 16:59:26 +0545

    stagger, keyframes added in about-us page

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../shared/todo';
 
@@ -7,10 +7,31 @@ import { Todo } from '../../shared/todo';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
   animations : [
-    trigger('fade',[
-      state('void',style({ opacity:0})),
-      transition('void <=> *',[
-        animate(2000)
+    // trigger('fade',[
+    //   state('void',style({ opacity:0})),
+    //   transition('void <=> *',[
+    //     animate(2000)
+    //   ])
+    // ]),
+    trigger('listAnimation',[
+      transition('* => *',[
+        query(':enter', style({opacity:0}), {optional:true}),
+
+        query(':enter', stagger('300ms',[
+          animate('1s ease-in',keyframes([
+            style({opacity:0,transform:'translateY(-75px)',offset:0}),
+            style({opacity:.5,transform:'translateY(35px)',offset:0.3}),
+            style({opacity:1,transform:'translateY(0)',offset:1})
+          ]))
+        ]),{ optional:true}),
+
+        query(':leave', stagger('300ms',[
+          animate('1s ease-in',keyframes([
+            style({opacity:0,transform:'translateY(0)',offset:0}),
+            style({opacity:.5,transform:'translateY(35px)',offset:0.3}),
+            style({opacity:1,transform:'translateY(-75px)',offset:1})
+         ]))
+         ]),{ optional:true})
       ])
     ])
   ]
