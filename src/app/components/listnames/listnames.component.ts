f7868e26fbc97256efc0c11b11ffce43f95987a0
Anushka-234 <anushkabhattarai@bajratechnologies.com>
2022-01-06 10:11:24 +0545

    list collapse feature added, task component for each tasks

--- src/app/components/listnames/listnames.component.ts
+++ src/app/components/listnames/listnames.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/shared/services/task.service';
 
 @Component({
   selector: 'app-listnames',
@@ -8,10 +9,33 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListnamesComponent implements OnInit {
   @Input() list:string;
   collapsed : boolean = true;
+  items:any;
+  tasks:any[];
 
-  constructor() { }
+  constructor(private service:TaskService) { }
 
   ngOnInit(): void {
+    this.getTasksofList(this.list);
   }
+
+
+  getTasksofList(listname : string){
+    this.service.getTasks().subscribe(
+      res => {
+        this.tasks = res;
+        this.tasks = this.tasks.filter((task:any) => task.list == this.list)
+      }
+
+    )
+
+  }
+
+deleteTask(task:any){
+  this.service
+  .deleteTask(task)
+  .subscribe(
+    () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+  );
+}
 
 }
