5f598e152308d1d4c3e58ee4a51c90c8d92d2404
Anushka-234 <anushkabhattarai@bajratechnologies.com>
2022-01-24 10:03:30 +0545

    tested list and task components

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -2,11 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ListComponent } from './list.component';
 import { ToastrModule } from 'ngx-toastr';
+import { TaskService } from 'src/app/shared/services/task.service';
 
 
 describe('ListnamesComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let service: TaskService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,5 +22,16 @@ describe('ListnamesComponent', () => {
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.get(TaskService);
+  });
+
+  it('should call getTasks on ngOnInit', () => {
+    const servicespy = spyOn(service, 'getTasks').and.callThrough();
+    const componentsspy = spyOn(component, 'getTasksofList').and.callThrough();
+    expect(servicespy).not.toHaveBeenCalled();
+    expect(componentsspy).not.toHaveBeenCalled();
+    component.ngOnInit();
+    expect(componentsspy).toHaveBeenCalledTimes(1);
+    expect(servicespy).toHaveBeenCalledTimes(1);
   });
 });
